289dad23e8c72901ee296876f5449ec642e7781c
ferdykod <36296514+ferdykod@users.noreply.github.com>
2018-06-18 15:10:23 +0200

    Create Computable.java

---
+++ src/main/java/cm/enspgrp1/dietProblem/user/Computable.java
@@ -0,0 +1,13 @@
+package cm.enspgrp1.dietProblem.user;
+
+import java.util.HashSet;
+
+public class Computable extends ComputableAdapter {
+
+	/*@Override
+	public void compute(HashSet<PersonByBracket> hashSetPerson) {
+		// TODO Auto-generated method stub
+		
+	}*/
+
+}
